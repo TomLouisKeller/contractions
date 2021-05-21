489b1ef00064873c2f36146eab957b76d745b45e
Tom Keller <tomlouiskeller@gmail.com>
2021-05-21 10:42:13 +0200

    Remove main.rs from being tracked

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::error::Error;
-
-use contractions::Contractions;
-
-fn main() -> Result<(), Box<dyn Error>> {
-    let contractions = Contractions::default();
-    print!("Library: {}", contractions.expand("I'm happy to meet you"));
-    Ok(())
-}
